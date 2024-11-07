8df7c54bc7cc80afb519ce3fa0332733dfda170f
David_Toas <showoleolabode.6@gmail.com>
2024-11-07 18:55:29 +0100

    start

---
+++ components/SideNav.tsx
@@ -0,0 +1,18 @@
+import Logo from "./Logo";
+
+function SideNav() {
+  return (
+    <div>
+      <div>
+        <Logo/>
+        {/* <NavLinks /> */}
+        {/* user && <profileLink /> */}
+        <div>
+            {/* <MoreDropdown/> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideNav;
